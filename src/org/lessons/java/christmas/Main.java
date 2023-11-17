fa82f0b9d02de657972165109ffec087ccf8b0d8
alep-2000 <alessiopiras056@gmail.com>
2023-11-17 12:46:02 +0100

    Richiesta all'utente del nome del regalo

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,7 +1,16 @@
 package org.lessons.java.christmas;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.println("Inserisci il nome del regalo:");
+		String strRegalo = in.nextLine();
+		
+		System.out.println(strRegalo);
+		
+		in.close();
 	}
 }
