180252e68b106f1ec9a2020fb3eb449e321cd72b
alep-2000 <alessiopiras056@gmail.com>
2023-11-17 13:09:06 +0100

    Inseriemento dei regali in una collection e stampa in console- esercizio base finito

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,16 +1,37 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Inserisci il nome del regalo:");
-		String strRegalo = in.nextLine();
+		List<String> lista = new ArrayList<>();
 		
-		System.out.println(strRegalo);
+		while(true) {
+			System.out.println("Inserisci il nome del regalo: ");
+			String strRegalo = in.nextLine();
+			
+			lista.add(strRegalo);
+			
+			System.out.println("Vuoi inserire un'altro regalo? (Si/No)");
+			String strNuovoRegalo = in.nextLine().toLowerCase();
+			
+			if(strNuovoRegalo.equals("no")) break;
+		}
 		
+		Collections.sort(lista);
+		
+		System.out.println("------------------------------");
+		
+		for(String regalo : lista) {
+			System.out.println("Regalo: " + regalo);
+		}
+		
+		System.out.println("-----------------------------");
 		in.close();
 	}
 }
